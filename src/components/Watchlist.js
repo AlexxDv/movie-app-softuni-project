80932fd63311c02b30d6ef958a53e276ec9b817e
AlexxDv <alexxdimov@gmail.com>
2023-03-24 19:27:18 +0100

    checking the length

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -8,14 +8,18 @@ export const Watchlist = () => {
     <div className="movie-page">
       <div className="container">
         <div className="header">
-            <h1 className="heading">My Watchlist</h1>
+          <h1 className="heading">My Watchlist</h1>
         </div>
 
-        <div className="movie-grid">
-          {watchlist.map(movie => (
-            <WatchListCard key={movie.id} movie={movie} type="watchlist" />
-          ))}
-        </div>
+        {watchlist.length > 0 ?
+          <div className="movie-grid">
+            {watchlist.map(movie => (
+              <WatchListCard key={movie.id} movie={movie} type="watchlist" />
+            ))}
+          </div>
+          :
+          <h2 className="no-movies">No movies in your list, you need to add some!</h2>
+        }
 
       </div>
     </div>
