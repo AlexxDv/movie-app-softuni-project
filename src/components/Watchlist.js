77405baa3e632be4b11e75f6039d5ad76d581646
AlexxDv <alexxdimov@gmail.com>
2023-03-24 19:23:04 +0100

    WatchListCard Added

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import { React, useContext } from 'react'
+import { GlobalContext } from './context/GlobalState';
+import { WatchListCard } from './context/WatchListCard';
 
 export const Watchlist = () => {
+  const { watchlist } = useContext(GlobalContext);
   return (
-    <div>
-      <h1>Watchlist Page</h1>
+    <div className="movie-page">
+      <div className="container">
+        <div className="header">
+            <h1 className="heading">My Watchlist</h1>
+        </div>
+
+        <div className="movie-grid">
+          {watchlist.map(movie => (
+            <WatchListCard key={movie.id} movie={movie} type="watchlist" />
+          ))}
+        </div>
+
+      </div>
     </div>
+
   )
 }
