d36415e0cdad228db85ab239d30fce3ab947c5ec
AlexxDv <alexxdimov@gmail.com>
2023-03-26 14:27:01 +0200

    Login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export const Login = () => {
+export const  Login =() => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Email: ${email}, Password: ${password}`);
+    // Here you can add the logic to submit the login form to your server or API
+  };
+
   return (
-    <h1>Login Page</h1>
-  )
+    <div className="login-form-container">
+      <form onSubmit={handleSubmit} className="login-form">
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          required
+        />
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          required
+        />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
+
