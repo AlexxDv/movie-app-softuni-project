bfd25a78b85669293d011a4e5581f03f1bc57d15
AlexxDv <alexxdimov@gmail.com>
2023-03-26 16:51:27 +0200

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate  } from 'react-router-dom';
 
 export const Register = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [repeatPassword, setRepeatPassword] = useState('');
+  const [isRegistered, setIsRegistered] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const handleSubmit = (event) => {
+  
+  const navigate  = useNavigate();
+
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
 
     if (!email || !password || !repeatPassword) {
@@ -19,49 +24,81 @@ export const Register = () => {
       setErrorMessage('Passwords do not match');
       return;
     }
+    const response = await fetch('http://localhost:3030/users/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
+    });
 
-    console.log(`Email: ${email}, Password: ${password}`);
-    // Here you can add the logic to submit the registration form to your server or API
+    if (!response.ok) {
+      const error = await response.json();
+      alert(error.message);
+      return;
+    }
+   
+
+    const data = await response.json();
+    const token = data.accessToken;
+
+    // Save the authorization token to local storage
+    localStorage.setItem('token', token);
+    if (token) {
+      setIsRegistered(true);
+      navigate('/')
+    }
+
   };
-
   return (
-    <div className="login-form-container">
-      <form onSubmit={handleSubmit} className="login-form">
-        <h2>Sign Up</h2>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-          required
-        />
+    <>
+      {isRegistered ?
+        (
+          <p>Successfully registered!</p>
+        ) : (
+          <div className="login-form-container">
+            <form onSubmit={handleSubmit} className="login-form">
+              <h2>Sign Up</h2>
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                required
+              />
 
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          required
-        />
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+                required
+              />
 
-        <label htmlFor="repeat-password">Repeat Password:</label>
-        <input
-          type="password"
-          id="repeat-password"
-          value={repeatPassword}
-          onChange={(event) => setRepeatPassword(event.target.value)}
-          required
-        />
+              <label htmlFor="repeat-password">Repeat Password:</label>
+              <input
+                type="password"
+                id="repeat-password"
+                value={repeatPassword}
+                onChange={(event) => setRepeatPassword(event.target.value)}
+                required
+              />
 
-        {errorMessage && <div className="error-message">{errorMessage}</div>}
+              {errorMessage && <div className="error-message">{errorMessage}</div>}
 
-        <button type="submit">Register</button>
-        <p>
-          Already registered? <Link to="/login">Login here</Link>.
-        </p>
-      </form>
-    </div>
+              <button type="submit">Register</button>
+              <p>
+                Already registered? <Link to="/login">Login here</Link>.
+              </p>
+            </form>
+          </div>
+        )
+      }
+    </>
   );
 }
