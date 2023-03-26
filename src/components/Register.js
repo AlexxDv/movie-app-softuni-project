923ad06ed3ae1abd342c42af5254cb0abaf2cb31
AlexxDv <alexxdimov@gmail.com>
2023-03-26 14:32:43 +0200

    Register added

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export const Register = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repeatPassword, setRepeatPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!email || !password || !repeatPassword) {
+      setErrorMessage('All fields are required');
+      return;
+    }
+
+    if (password !== repeatPassword) {
+      setErrorMessage('Passwords do not match');
+      return;
+    }
+
+    console.log(`Email: ${email}, Password: ${password}`);
+    // Here you can add the logic to submit the registration form to your server or API
+  };
+
   return (
-    <>
-    <h1>Register Page</h1>
-  
-    </>)
+    <div className="login-form-container">
+      <form onSubmit={handleSubmit} className="login-form">
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          required
+        />
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          required
+        />
+
+        <label htmlFor="repeat-password">Repeat Password:</label>
+        <input
+          type="password"
+          id="repeat-password"
+          value={repeatPassword}
+          onChange={(event) => setRepeatPassword(event.target.value)}
+          required
+        />
+
+        {errorMessage && <div className="error-message">{errorMessage}</div>}
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 }
